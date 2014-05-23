76f97c4bf9c836f2383ffcc1ca6b97958d88c4a8
xouabita <xouabita@gmail.com>
2014-05-23 18:53:45 +0200

    fix inputs for validation

--- src/documents/scripts/app.js
+++ src/documents/scripts/app.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+  // Toggle the menu
   document.getElementById('open_menu').onclick = function () {
     console.log("Clicked");
     var mobile_menu = document.getElementById('mobile_menu');
@@ -9,4 +10,32 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     return false;
   };
+
+  function fn () {
+    var placeholders = document.getElementsByClassName("placeholder");
+    for (var i = 0; i < placeholders.length; i++) {
+      var label = placeholders[i];
+      if (label.getAttribute("for") === this.id) {
+        if (this.value === "") {
+          label.style.visibility = 'visible';
+        } else {
+          label.style.visibility = 'hidden';
+        }
+      }
+    }
+  }
+
+  // Replace placeholder by label
+  var placeholders = document.getElementsByClassName("placeholder");
+  for (var i = 0; i < placeholders.length; i++) {
+    var label     = placeholders[i];
+    var target_id = label.getAttribute("for");
+    var target    = document.getElementById(target_id);
+
+    target.addEventListener('keydown', fn);
+    target.addEventListener('keypress', fn);
+    target.addEventListener('keyup', fn);
+    target.addEventListener('focus', fn);
+    target.addEventListener('blur', fn);
+  }
 }, false);
